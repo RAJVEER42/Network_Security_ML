552ea315fac846d084da1f798b01a2a83bca8167
RAJVEER42 <irajveer.bishnoi2310@gmail.com>
2025-12-30 05:16:30 +0530

    Data Tranformation completed

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -26,8 +26,4 @@ class ClassificationMetricArtifact:
     precision_score: float
     recall_score: float
     
-@dataclass
-class ModelTrainerArtifact:
-    trained_model_file_path: str
-    train_metric_artifact: ClassificationMetricArtifact
-    test_metric_artifact: ClassificationMetricArtifact
+
