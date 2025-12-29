6fa5af409f1dc1ab4b498fa164300cb1fad7f8f6
RAJVEER42 <irajveer.bishnoi2310@gmail.com>
2025-12-30 04:48:53 +0530

    Data Validation is completed

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,9 +3,12 @@ import sys
 import numpy as np
 import pandas as pd
 
+
 """
 defining common constant variable for training pipeline
 """
+
+
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
@@ -24,9 +27,23 @@ MODEL_FILE_NAME = "model.pkl"
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
 
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "RajveerDB"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
